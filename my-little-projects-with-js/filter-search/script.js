0691f9044e29979a1e444d5e71bffff93446d06b
Maksim Mishchenko <programm532@ukr.net>
2020-05-07 21:14:35 +0300

    fix: исправлен баг отображения списка на IOS

--- my-little-projects-with-js/filter-search/script.js
+++ my-little-projects-with-js/filter-search/script.js
@@ -1,9 +1,13 @@
 let searchField = document.querySelector(".filter-search__input");
+let containerHeight = document.querySelector(".filter-search-container").offsetHeight;
+
 
 function searchElements(e) {
-
+    
     let value = e.target.value.toLowerCase().trim();
     let allElements = document.querySelectorAll(".filter-search__list-element");
+
+    document.querySelector(".filter-search-container").style.height = containerHeight + "px";
 
     allElements.forEach(element => {
 
