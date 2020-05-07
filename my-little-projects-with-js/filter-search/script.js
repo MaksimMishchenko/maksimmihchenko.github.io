3f3bb5bdb4f501927f9f06c8c4ed92a8d7e3021e
Maksim Mishchenko <programm532@ukr.net>
2020-05-07 20:36:27 +0300

    add: Добавлен проект filter-search в my-little-projects-with-js

---
+++ my-little-projects-with-js/filter-search/script.js
@@ -0,0 +1,22 @@
+let searchField = document.querySelector(".filter-search__input");
+
+
+function searchElements(e) {
+
+    let value = e.target.value.toLowerCase().trim();
+    let allElements = document.querySelectorAll(".filter-search__list-element");
+
+    allElements.forEach(element => {
+
+        if(!element.textContent.toLowerCase().includes(value)) {
+            element.style.display = "none";
+        } else {
+            element.style.display = "block";
+        }
+
+    });
+
+}
+
+
+searchField.addEventListener("input", searchElements);
